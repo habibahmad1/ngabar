538ede617db820d180e4713d9fe447d84014f941
habibahmad1 <oregairu2597@gmail.com>
2024-04-05 11:35:49 +0700

    fix navbar

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -1,10 +1,9 @@
 @extends('./layouts/main')
 @section('content')
 <h1>Halaman Artikel</h1>
-@foreach ($article as $post)
-    
-<h2>{{ $post['judul']; }}</h2>
-<h5>{{ $post['penulis']; }}</h5>
-<p>{{ $post['artikelPost']; }}</p>
-@endforeach
+    @foreach ($article as $post)
+        <h2>{{ $post['judul'] }}</h2>
+        <h5>{{ $post['penulis'] }}</h5>
+        <p>{{ $post['artikelPost'] }}</p>
+    @endforeach
 @endsection
